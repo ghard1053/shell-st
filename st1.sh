d9d26e720e607cb15d5748fd0f4926930d1fe839
Takao <sukisya70@gmail.com>
2019-03-11 23:58:16 +0900

    190311

--- st1.sh
+++ st1.sh
@@ -173,3 +173,28 @@ done
   who
 } > logfile
 
+
+func()
+{
+  echo 'do'
+}
+func
+
+ll()
+{
+  ls -l "$@"
+}
+
+factorial()
+{
+  if [ "$1" -le 1 ]; then
+    echo 1
+    return
+  fi
+
+  n=`expr "$1" -1`
+  n=`factorial "$n"`
+  expr "$n" \* "$1"
+}
+
+factorial "$1"
