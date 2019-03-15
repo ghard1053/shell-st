3f58f9bf0505694998ab68cc1c8281fe66f9fc43
Takao <sukisya70@gmail.com>
2019-03-15 23:40:22 +0900

    190315

--- st2.sh
+++ st2.sh
@@ -28,3 +28,59 @@ source file
 eval echo \"\$$var\"
 
 exec myprog
+
+if [ ! -f "$1" ]
+then
+  echo "$1"'ファイルが存在しません'
+  exit 1
+fi
+cp -p "$1" "$1".bak
+exit 0
+
+LANG=ja_JP.eucJP
+export LANG
+
+TEXT=hello
+echo "$TEXT"
+printenv TEXT
+export TEXT
+printenv TEXT
+
+export LANG
+LANG=ja_JP.eucJP
+
+CFLAGS='-02 -fomit-frame-pointer'
+LANG=ja_JP.eucJP
+export CFLAGS LANG
+
+TEXT=hello; export TEXT
+printenv TEXT
+# hello
+unset TEXT
+printenv TEXT
+echo "$TEXT"
+
+export LANG=ja_JP.eucJP
+
+echo -n 'prompt> ' 1>&2
+read input
+
+DIR=/usr/local
+readonly DIR
+
+func()
+{
+  if [ ! -f "$1" ]; then
+    echo "$1"'ファイルが存在しません'
+    return 1
+  fi
+  cp -p "$1" "$1".bak
+  return 0
+}
+
+while [ $# -gt 0 ]
+do
+  echo "$1"
+  shift
+done
+
