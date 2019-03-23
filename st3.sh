9a9c55ea13a15a7ac47fe40619c54a730a1bc78b
Takao <sukisya70@gmail.com>
2019-03-23 15:55:13 +0900

    190323

--- st3.sh
+++ st3.sh
@@ -9,6 +9,7 @@ else
   cp file /tmp
 fi
 
+
 cp file "${TMPDIR:=/tmp}"
 
 if [ -z "$TMPDIR" ]
@@ -19,3 +20,61 @@ cp file "$TMPDIR"
 
 : ${TMPDIR:=/tmp}
 cp file "$TMPDIR"
+
+
+cp "${1:?notfound}" /tmp
+
+if [ -z "$1" ]
+then
+  echo 'not found'
+  exit 1
+fi
+cp "$1" /tmp
+
+: ${1:?notfound}
+cp "$1" /tmp
+
+
+LD_LIBRARY_PATH=/usr/local/myapp/lib${LD_LIBRARY_PATH:+:}$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH
+
+
+dir=/usr/local/bin
+echo "${dir##*/}"
+# bin
+basename "$dir"
+# bin
+
+file=backup.tar.gz
+echo "${file#*.}"
+# tar.gz
+echo "${file##*.}"
+# gz
+
+
+dir=/usr/local/bin
+echo "${dir%/*}"
+# /usr/local
+dirname "$dir"
+# /usr/local
+
+file=backup.tar.gz
+echo "${file%.*}"
+# backup.tar
+echo "${file%%.*}"
+# backup
+
+
+message='Hello World'
+echo "${message:2}"
+# llo World
+echo "${message:2:5}"
+# llo W
+
+
+message='Hello World'
+echo "${message/l/X}"
+# HeXlo World
+echo "${message//l/X}"
+# HeXXo WorXd
+
